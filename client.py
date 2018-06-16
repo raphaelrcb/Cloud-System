958140d1d5d7a1489edd0a6a816a1bbc5c38dd5a
Unknown <raphaelrcbarbosa@gmail.com>
2018-06-16 16:28:43 -0300

    Authentication works in client and server code
    As client connects, the server asks for client's authentication

--- client.py
+++ client.py
@@ -1,11 +1,16 @@
 import socket
+import sys
+import authenticate
 
 def Connected(client_socket, msg):
+    user, password, login = authenticate.client_login(sys.argv, client_socket)
+    print 'Seja bem vindo ', user, "!\n Execute o comando 'help' para mais opcoes:"
+    print 'Para sair use CTRL+X\n'
     while msg <> '\x18':
-        
+        # server_msg = client_socket.recv(1024)
+        # print server_msg
         client_socket.send (msg)
         msg = raw_input()
-
     return
 
 
@@ -16,13 +21,13 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (HOST, PORT)
 
 client_socket.connect(dest)
-print 'Para sair use CTRL+X\n'
-
 hand_shake = client_socket.recv(1024)
-
 msg = 'Hello Server'
 if hand_shake == 'OK':
     print 'Connection Stablished'
     Connected(client_socket, msg)
+else:
+    print 'No connection'
 
+print 'Client closed connection'
 client_socket.close()
--- server.py
+++ server.py
@@ -1,22 +1,45 @@
 import socket
-HOST = ''              # Endereco IP do Servidor
-PORT = 5001            # Porta que o Servidor esta
+import threading
+import thread
+import sys
+import authenticate
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-orig = (HOST, PORT)
-server_socket.bind(orig)
-server_socket.listen(1)
-print 'Waiting Connection...'
-while True:
-    connect, cliente = server_socket.accept()
+def thread_iniciated(connect, client):
     connect.send('OK')
+    print 'Concetado por', client
 
-    print 'Concetado por', cliente
+    user,password,login = authenticate.server_login(connect,client)
+    #print user, password, login
+    if login == 0:
+        failed = 'log in not succesfull, please try again'
+        #print failed
+        connect.send(failed)
+        quit()
+    # if login == 1:
+    #     print 'log in succesfull'
     while True:
         msg = connect.recv(1024)
         if not msg: break
-        print cliente, msg
+        print client, msg
+
     connect.close()
-    break
+    print 'saiu do loop'
+    return 'Close'
+
+HOST = ''              # Endereco IP do Servidor
+PORT = 5001            # Porta que o Servidor esta
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+orig = (HOST, PORT)
+
+print 'Waiting Connection...'
+server_socket.bind(orig)
+server_socket.listen(1)
+
+while True:
+    connect, client = server_socket.accept()
+    try:
+        msg = thread.start_new_thread( thread_iniciated,(connect, client))
+    except:
+        print 'could not create thread'
 
 print 'Finalizando conexao do cliente', cliente
